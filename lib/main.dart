80476c90dd0009e826e3d632bcd86ac45155b78c
heathkev <kevintuazon45@gmail.com>
2020-09-16 12:10:17 +0800

    import english word packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:english_words/english_words.dart';
+
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
@@ -8,7 +10,7 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(child: Text(nouns.first)),
         ),
       ),
     );
