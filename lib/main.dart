a7a29ed6712daf54327617a6a6e1e7d2b5ee7f80
heathkev <kevintuazon45@gmail.com>
2020-09-22 12:10:02 +0800

    Update key generate function

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,13 @@ class XylophoneApp extends StatelessWidget {
     player.play('note$number.wav');
   }
 
-  // TODO : Update method
-  void buildKey(){
-    Expanded(
+  Expanded buildKey({int noteNumber, Color buttonColor}){
+    return Expanded(
       child: FlatButton(
         onPressed:() {
-          playSound(1);
+          playSound(noteNumber);
         },
-        color: Colors.red,
+        color: buttonColor,
       ),
     );
   }
@@ -31,13 +30,13 @@ class XylophoneApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              // buildKey(),
-              // buildKey(),
-              // buildKey(),
-              // buildKey(),
-              // buildKey(),
-              // buildKey(),
-              // buildKey(),
+              buildKey(noteNumber: 1, buttonColor: Colors.red),
+              buildKey(noteNumber: 2, buttonColor: Colors.orange),
+              buildKey(noteNumber: 3, buttonColor: Colors.yellow),
+              buildKey(noteNumber: 4, buttonColor: Colors.green),
+              buildKey(noteNumber: 5, buttonColor: Colors.teal),
+              buildKey(noteNumber: 6, buttonColor: Colors.blue),
+              buildKey(noteNumber: 7, buttonColor: Colors.purple),
             ],
           ),
         ),
